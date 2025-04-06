ac5a77097e57c76c8b97d5c42a97b24cbba1a30a
Endy3032 <endy3032@icloud.com>
2025-04-07 00:31:58 +0700

    full asl site

--- Web/islands/Predictor.tsx
+++ Web/islands/Predictor.tsx
@@ -4,13 +4,76 @@ import { useEffect, useRef } from "preact/hooks"
 import { KDTree } from "../utils/KDTree.ts"
 
 export default function Predictor() {
-	const ready = useSignal(false)
+	const mpReady = useSignal(false)
+	const camReady = useSignal(false)
+	const aslReady = useSignal(true)
+
 	const camRef = useRef<HTMLVideoElement | null>(null)
 	const drawRef = useRef<HTMLCanvasElement | null>(null)
 	const predictor = useSignal<KDTree | null>(null)
 	const prediction = useSignal<string | null>(null)
 	let handLandmarker: HandLandmarker | null = null
+
+	const throttleMs = useSignal(1000)
 	let lastVideoTime = -1
+	let lastPredictionTime = 0
+
+	const history = useSignal<{ timestamp: Date; text: string }[]>([])
+	const sentence = useSignal("")
+
+	const processWebcam = () => {
+		const cam = camRef.current
+		const draw = drawRef.current
+		if (!cam || !draw || !handLandmarker) {
+			requestAnimationFrame(processWebcam)
+			return
+		}
+
+		if (cam.videoWidth === 0 || cam.videoHeight === 0) {
+			requestAnimationFrame(processWebcam)
+			return
+		}
+
+		if (draw.width !== cam.videoWidth || draw.height !== cam.videoHeight) {
+			draw.width = cam.videoWidth
+			draw.height = cam.videoHeight
+		}
+
+		const ctx = draw.getContext("2d")
+		if (!ctx) {
+			requestAnimationFrame(processWebcam)
+			return
+		}
+
+		const startTimeMs = performance.now()
+		if (lastVideoTime !== cam.currentTime) {
+			lastVideoTime = cam.currentTime
+			const results = handLandmarker.detectForVideo(cam, startTimeMs)
+
+			ctx.clearRect(0, 0, draw.width, draw.height)
+			const drawer = new DrawingUtils(ctx)
+			drawLandmarks(drawer, results)
+
+			if (results.landmarks.length === 0) {
+				prediction.value = null
+				requestAnimationFrame(processWebcam)
+				return
+			}
+
+			const data = [...results.worldLandmarks[0]?.reduce((acc, cur) => {
+				acc.push(cur.x, cur.y, cur.z)
+				return acc
+			}, [] as number[]), results.handedness[0]?.[0].index]
+
+			if (performance.now() - lastPredictionTime > throttleMs.value && aslReady.value) {
+				prediction.value = String.fromCharCode(predict(data) ?? 0)
+				if (prediction.value && sentence.value.at(-1) !== prediction.value) sentence.value += prediction.value
+				lastPredictionTime = performance.now()
+			}
+		}
+
+		requestAnimationFrame(processWebcam)
+	}
 
 	const predict = (landmarks: number[]) => {
 		if (!predictor.value) return null
@@ -28,66 +91,32 @@ export default function Predictor() {
 		})
 	}
 
-	const predictWebcam = async () => {
-		const cam = camRef.current
-		const draw = drawRef.current
-		if (!cam || !draw || !handLandmarker) {
-			requestAnimationFrame(predictWebcam)
-			return
-		}
-
-		if (cam.videoWidth === 0 || cam.videoHeight === 0) {
-			requestAnimationFrame(predictWebcam)
-			return
-		}
-
-		if (draw.width !== cam.videoWidth || draw.height !== cam.videoHeight) {
-			draw.width = cam.videoWidth
-			draw.height = cam.videoHeight
-		}
-
-		const ctx = draw.getContext("2d")
-		if (!ctx) {
-			requestAnimationFrame(predictWebcam)
-			return
-		}
-
-		const startTimeMs = performance.now()
-		if (lastVideoTime !== cam.currentTime) {
-			lastVideoTime = cam.currentTime
-			const results = await handLandmarker.detectForVideo(cam, startTimeMs)
-
-			ctx.clearRect(0, 0, draw.width, draw.height)
-			const drawer = new DrawingUtils(ctx)
-			drawLandmarks(drawer, results)
-
-			if (results.landmarks.length === 0) {
-				prediction.value = "No hands detected"
-				requestAnimationFrame(predictWebcam)
-				return
-			}
-
-			const data = [...results.worldLandmarks[0]?.reduce((acc, cur) => {
-				acc.push(cur.x, cur.y, cur.z)
-				return acc
-			}, [] as number[]), results.handedness[0]?.[0].index]
-
-			prediction.value = String.fromCharCode(predict(data) ?? 0)
-		}
-
-		requestAnimationFrame(predictWebcam)
-	}
-
 	useEffect(() => {
 		const startCamera = async () => {
 			try {
 				const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-				if (camRef.current) camRef.current.srcObject = stream
+				if (camRef.current) {
+					camRef.current.srcObject = stream
+					camReady.value = true
+					if (!drawRef.current) return
+					drawRef.current.width = camRef.current.videoWidth
+				}
 			} catch (error) {
 				console.error("Error accessing camera:", error)
 			}
 		}
 
+		startCamera()
+
+		return () => {
+			if (camRef.current?.srcObject) {
+				const stream = camRef.current.srcObject as MediaStream
+				stream.getTracks().forEach(track => track.stop())
+			}
+		}
+	}, [drawRef.current, camRef.current])
+
+	useEffect(() => {
 		const loadModel = async () => {
 			if (predictor.value) return
 			const response = await fetch("/model.bin")
@@ -109,44 +138,128 @@ export default function Predictor() {
 					runningMode: "VIDEO",
 					numHands: 1,
 				})
-				ready.value = true
-				requestAnimationFrame(predictWebcam)
+				mpReady.value = true
+				requestAnimationFrame(processWebcam)
 			} catch (error) {
 				console.error("Failed to initialize HandLandmarker:", error)
 			}
 		}
 
-		startCamera()
 		loadModel()
 		createHandLandmarker()
-
-		return () => {
-			if (camRef.current?.srcObject) {
-				const stream = camRef.current.srcObject as MediaStream
-				stream.getTracks().forEach(track => track.stop())
-			}
-		}
 	}, [])
 
 	return (
-		<main class="flex-1 flex flex-col lg:flex-row gap-4 p-4">
-			<div className="flex flex-1 flex-col bg-zinc-700 rounded-xl p-2">
-				<div class="relative w-full h-max">
-					<video ref={camRef} autoPlay playsInline id="cam" class="w-full h-auto rounded-md" />
-					<canvas ref={drawRef} id="draw" class="absolute top-0 w-full h-full" />
+		<main class="w-full h-fit flex-1 flex flex-col lg:flex-row gap-4 p-4 max-w-[2160px] mx-auto">
+			<div className="flex lg:w-max h-fit flex-col bg-zinc-700 rounded-3xl gap-2 p-4 lg:max-w-xl sticky top-4">
+				<img src="/asl.jpg" alt="ASL Instructions" class="rounded-t-xl rounded-b max-h-64 lg:max-h-max w-auto object-contain" />
+				<div class="relative flex justify-center w-full h-auto max-h-64 lg:max-h-max aspect-video bg-zinc-600/50 rounded-t rounded-b-xl overflow-hidden">
+					<video ref={camRef} autoPlay playsInline id="cam" class="absolute h-full w-auto" />
+					<canvas ref={drawRef} id="draw" class="absolute top-0 h-full" />
+				</div>
+				<div className="flex flex-col gap-4">
+					<div className="flex flex-col gap-1 mt-2 text-zinc-400 px-4">
+						<label class="ms-2 text-lg" htmlFor="speed">Độ trễ nhận diện: {throttleMs.value / 1000}s</label>
+						<div className="flex items-center gap-2 justify-between">
+							<span class="text-sm tabular-nums">0s</span>
+							<input
+								type="range"
+								id="speed"
+								min="0"
+								max="2000"
+								step="100"
+								defaultValue={throttleMs.value.toString()}
+								className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+								onInput={e => {
+									const value = parseInt((e.target as HTMLInputElement).value)
+									throttleMs.value = value
+									lastPredictionTime = performance.now()
+								}}
+							/>
+							<span class="text-sm tabular-nums">2s</span>
+						</div>
+					</div>
+					<div className="grid grid-cols-2 lg:grid-cols-4 justify-between gap-2">
+						<button
+							type="button"
+							class="rounded-md px-4 py-3 bg-zinc-600 hover:bg-zinc-500 transition-all outline-none focus:ring focus:ring-zinc-300"
+							onClick={() => sentence.value = sentence.value.replace(/#$/, "").slice(0, -1)}
+						>
+							Xóa chữ cuối
+						</button>
+						<button
+							type="button"
+							class="rounded-md px-4 py-3 bg-zinc-600 hover:bg-zinc-500 transition-all outline-none focus:ring focus:ring-zinc-300"
+							onClick={() => sentence.value = ""}
+						>
+							Xóa câu
+						</button>
+						<button
+							type="button"
+							class="rounded-md px-4 py-3 bg-zinc-600 hover:bg-zinc-500 transition-all outline-none focus:ring focus:ring-zinc-300"
+							onClick={() => {
+								if (sentence.value.replaceAll("#", "").trim().length === 0) return
+								history.value.push({ timestamp: new Date(), text: sentence.value.replaceAll("#", "") })
+								sentence.value = ""
+							}}
+						>
+							Lưu câu
+						</button>
+						<button
+							type="button"
+							class="rounded-md px-4 py-3 bg-zinc-600 hover:bg-zinc-500 transition-all outline-none focus:ring focus:ring-zinc-300"
+							onClick={() => {
+								aslReady.value = !aslReady.value
+								if (aslReady.value) {
+									prediction.value = null
+									lastPredictionTime = performance.now()
+								}
+							}}
+						>
+							{aslReady.value ? "Tạm dừng" : "Tiếp tục"}
+						</button>
+					</div>
+					<div className="flex gap-4 ps-2">
+						<div className="flex items-center gap-2">
+							<span class={`status ${camReady.value ? "active" : "inactive"}`}></span>Camera
+						</div>
+						<div className="flex items-center gap-2">
+							<span class={`status ${mpReady.value ? "active" : "inactive"}`}></span>Nhận diện tay
+						</div>
+						<div className="flex items-center gap-2">
+							<span class={`status ${aslReady.value ? "active" : "inactive"}`}></span>Dự đoán ASL
+						</div>
+					</div>
 				</div>
 			</div>
-			<div className="flex-1">
-				{ready.value
-					? (
-						<>
-							<p>Hand Tracking Active</p>
-							<p>{prediction.value || "Waiting for prediction..."}</p>
-						</>
-					)
-					: (
-						"Loading Model..."
-					)}
+			<div className="flex flex-1 flex-col bg-zinc-700 rounded-3xl gap-2 p-4">
+				<div className="sticky bottom-4 order-last items-end lg:items-start lg:order-first lg:top-4 flex gap-2">
+					<div key={prediction.value} className="flex rounded-xl justify-center items-center w-32 h-12 bg-zinc-600 animate-flash">
+						Dự đoán: {prediction.value || "ø"}
+					</div>
+					<button type="button"
+						class="rounded-xl h-12 bg-zinc-600 hover:bg-zinc-500 transition-all outline-none focus:ring focus:ring-zinc-300 px-4 py-3"
+						onClick={() => sentence.value = (sentence.value.replace(/[# ]$/, "") + " ").trimStart()}
+					>
+						␣
+					</button>
+					<textarea
+						className={`min-h-12 flex-1 break-words max-w-full block h-auto rounded-xl bg-zinc-600 px-4 py-3 ${
+							sentence.value.length === 0 ? "text-zinc-300" : ""
+						}`}
+						value={sentence.value.replaceAll("#", "")}
+						placeholder="Aa"
+					>
+					</textarea>
+				</div>
+				<div className="flex-1 flex flex-col gap-2 max-h-full overflow-scroll">
+					{history.value.map(item => (
+						<div key={item.timestamp.toString()} className="flex gap-2">
+							<span className="text-zinc-400 tabular-nums">{item.timestamp.toLocaleTimeString()}</span>
+							<span className="text-zinc-200">{item.text}</span>
+						</div>
+					))}
+				</div>
 			</div>
 		</main>
 	)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,21 @@ export default {
 	content: [
 		"Web/{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
 	],
+	theme: {
+		extend: {
+			animation: {
+				flash: "flash 0.5s ease-in-out",
+			},
+			keyframes: {
+				flash: {
+					"0%": {
+						filter: "brightness(1.2)",
+					},
+					"100%": {
+						filter: "brightness(1)",
+					},
+				},
+			},
+		},
+	},
 } satisfies Config
